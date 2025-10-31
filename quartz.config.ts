940adfaf4adac34a5de3850f219b7332dfbdb562
social lensiro <social@lensiro.com>
2025-10-31 17:24:08 +0700

    Auto backup Fri Oct 31 17:24:08 WIB 2025

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Manual Lensiro",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
